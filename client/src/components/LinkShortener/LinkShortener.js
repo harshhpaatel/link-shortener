40dd9b13b7470780b03edce1be713a5ff5f550ef
Harsh Patel <contact@harshpatel.ca>
2020-10-15 17:11:25 -0400

    Total Links working

--- client/src/components/LinkShortener/LinkShortener.js
+++ client/src/components/LinkShortener/LinkShortener.js
@@ -6,9 +6,19 @@ import AllLinks from "../AllLinks/AllLinks";
 import axios from "axios";
 
 class LinkShortener extends Component {
+  constructor(props) {
+    super(props);
+
+    this.refresh = this.refresh.bind(this);
+  }
+
   state = {
     data: [],
   };
+
+  refresh() {
+    this.getLinks();
+  }
 
   componentDidMount() {
     this.getLinks();
@@ -32,10 +42,12 @@ class LinkShortener extends Component {
       <div>
         <div className="container">
           <h2 className="page-title mb-0">Dashboard</h2>
-          <p className="page-subtitle code">Total Links: 2</p>
+          <p className="page-subtitle code">
+            Total Links: {this.state.data.length}
+          </p>
         </div>
         <div className="container mt-5">
-          <NewLinkForm />
+          <NewLinkForm refresh={this.refresh} />
         </div>
         <div className="container mt-5">
           <AllLinks data={this.state.data} />
